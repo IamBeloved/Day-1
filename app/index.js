2a198abef095ff81b169a2b7d1fb1d1d3eea1ffc
unknown <ajijolaonpoint@gmail.comgit config --global user.name IamBeloved>
2017-05-08 21:47:13 +0100

    add application file and test file

---
+++ app/index.js
@@ -0,0 +1,14 @@
+function getPrimes(num){
+  for (var counter = 1; counter <= num; counter++){
+    var not_prime = false;
+    for (var div = 2; div <= (counter - 1); div++) 
+    {
+        if (counter % div === 0) 
+        {not_prime = true;}
+    }
+    if (not_prime === false) 
+    {console.log([counter]);}
+    
+}
+}
+getPrimes(10)
